24085f8f9968be0cf95456760baa419bc661a951
mikoker <mikolaj.kolata@gmail.com>
2025-10-16 12:05:09 +0200

    sweet: init evolution-x 11.x

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -15,6 +15,22 @@ $(call inherit-product, device/xiaomi/sweet/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Evolution X Flags
+ifeq ($(WITH_GMS),true)
+    TARGET_USES_MINI_GAPPS := true
+    PRODUCT_PACKAGES += \
+        PixelWallpapers2025 \
+        SweetUpdaterOverlay
+else
+    PRODUCT_PACKAGES += \
+        SweetUpdaterVanillaOverlay
+endif
+
+TARGET_INCLUDE_ACCORD := false
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
+TARGET_SUPPORTS_64_BIT_APPS := true
+BYPASS_CHARGE_SUPPORTED := true
+
 PRODUCT_NAME := lineage_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
