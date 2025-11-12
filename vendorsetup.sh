3f82395c12c2548d98b4315b665e3bbf68318f98
QQuantum <130094755+qquantum89@users.noreply.github.com>
2025-11-12 20:36:27 +0530

    Update vendor and device tree URLs in vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,20 +6,20 @@
 #git clone -b rebase --depth=1 https://github.com/TheHewra/device_xiaomi_sweet device/xiaomi/sweet
 
 echo "Cloning vendor tree..."
-git clone --depth=1 https://github.com/TheHewra/vendor_xiaomi_sweet vendor/xiaomi/sweet
+git clone --depth=1 https://github.com/Sweet-Reborn/vendor_xiaomi_sweet.git vendor/xiaomi/sweet
 
 echo "Cloning sm6150-common device tree..."
 git clone -b rebase --depth=1 https://github.com/Sweet-Reborn/device_xiaomi_sm6150-common device/xiaomi/sm6150-common
 
 echo "Cloning sm6150-common vendor tree..."
-git clone --depth=1 https://github.com/TheHewra/vendor_xiaomi_sm6150-common vendor/xiaomi/sm6150-common
+git clone --depth=1 https://github.com/Sweet-Reborn/vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
 
 echo "Cloning kernel..."
-git clone --depth=1 https://github.com/TheHewra/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone --depth=1 https://github.com/Sweet-Reborn/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
 
 echo "Cloning MiuiCamera trees..."
-git clone --depth=1 https://github.com/TheHewra/device_xiaomi_miuicamera-sweet device/xiaomi/miuicamera-sweet
-git clone -b miuicamera --depth=1 https://github.com/TheHewra/vendor_xiaomi_miuicamera-sweet vendor/xiaomi/miuicamera-sweet
+git clone --depth=1 https://github.com/Sweet-Reborn/device_xiaomi_miuicamera-sweet2.git device/xiaomi/miuicamera-sweet
+git clone -b miuicamera --depth=1 https://github.com/Sweet-Reborn/vendor_xiaomi_miuicamera-sweet2.git vendor/xiaomi/miuicamera-sweet
 
 echo "Cloning hardware repos..."
 git clone -b bka-no-dolby --depth=1 https://github.com/Evolution-X-Devices/hardware_xiaomi hardware/xiaomi
