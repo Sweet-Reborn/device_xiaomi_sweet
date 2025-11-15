e3e116db3301bf0fb4fa8036b8ae44f765d4dd1a
QQuantum <130094755+qquantum89@users.noreply.github.com>
2025-11-15 10:58:08 +0530

    Remove Axion OS specific configurations

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -15,36 +15,6 @@ $(call inherit-product, device/xiaomi/sweet/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Axion Os
-TARGET_DISABLE_EPPE := true
-TARGET_ENABLE_BLUR := true
-TARGET_INCLUDE_VIPERFX := true
-# Camera information (multiple sensors supported)
-AXION_CAMERA_REAR_INFO := 64,8,5,2
-AXION_CAMERA_FRONT_INFO := 16
-
-# Maintainer name (underscores become spaces in the UI)
-AXION_MAINTAINER := dodo_Lx
-
-# Processor name (underscores become spaces)
-AXION_PROCESSOR := Snapdragon_732G_(8nm)
-BYPASS_CHARGE_SUPPORTED := true
-# Lunaris Os Flags
-WITH_GMS := true
-WITH_GAPPS := true
-TARGET_NEEDS_DOZE_FIX := true
-TORCH_STR_SUPPORTED := true
-AXION_CPU_PRIME_CORES := 7
-AXION_CPU_BIG_CORES := 4,5,6
-AXION_CPU_SMALL_CORES := 0,1,2,3
-TARGET_BOOT_ANIMATION_RES := 1080
-
-
-PRODUCT_SYSTEM_PROPERTIES += \
-    persist.sys.axion_cpu_prime=$(AXION_CPU_PRIME_CORES) \
-    persist.sys.axion_cpu_big=$(AXION_CPU_BIG_CORES) \
-    persist.sys.axion_cpu_small=$(AXION_CPU_SMALL_CORES)
-
 TARGET_INCLUDE_ACCORD := true
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
 TARGET_SUPPORTS_64_BIT_APPS := true
@@ -82,3 +52,17 @@ PRODUCT_NO_CAMERA := true
 
 # Whether to ship lawnchair launcher, false by default
 TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false 
+# GMS build flags, true by default
+# Ship with GMS packages, replaces default AOSP packages with Google manufactured packages.
+WITH_GMS := true/false
+
+# CORE build flags
+WITH_GMS := true
+TARGET_USES_PICO_GAPPS := true
+
+# VANILLA only build flags
+#WITH_GMS := false
+
+# VANILLA build with MICROG
+#WITH_GMS := false
+#WITH_MICROG := true
