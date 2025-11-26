6da76e0846cd379cb2647c9d3e1acafd6d3bc5a8
QQuantum <130094755+qquantum89@users.noreply.github.com>
2025-11-26 19:30:33 +0530

    Integrate ReVanced into device.mk
    Added ReVanced integration to the device.mk file.

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 # MiuiCamera
 $(call inherit-product-if-exists, device/xiaomi/miuicamera-sweet/device.mk)
 
+# ReVanced integration
+$(call inherit-product, vendor/revanced/products/revanced.mk)
+
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 30
 
